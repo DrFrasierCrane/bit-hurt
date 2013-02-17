d4fd3aa324778edabdf4423030173f82bdad49d3
DrFrasierCrane <tylersartin@gmail.com>
2013-02-17 15:28:13 -0500

    Simplified and reduced.

--- dec2bin.py
+++ dec2bin.py
@@ -1,5 +1,8 @@
-from sys import exit
-
+# Main function to create binary from decimal input. Function takes input float
+# and divides by 2. If the answer is even, the least significant bit (which will be the
+# first bit in the list) is 0. If answer is odd, the LSB is 1.
+# Note if answer is odd, we must round down using int, and convert back to float to keep
+# looping.
 def create(num):
 	while num > 0:
 		if num/2 == int(num/2):
@@ -9,6 +12,9 @@ def create(num):
 			binlist.append('1')
 			num = float(int(num / 2))
 
+
+# Function to create binary list and also to check if the original number entered
+# is zero. If not proceed to function create.
 def convert(num):
 	global binlist
 	binlist = []
@@ -17,11 +23,12 @@ def convert(num):
 		print 0
 	else:
 		create(num)
-	
+
+
 num = float(raw_input("> "))
 
 convert(num)
 
-answer = ''.join(binlist)
-
-print answer[::-1]
+# Print answer which is a sum of all string of bits. The string is then printed in
+# reverse for correct binary conversion.
+print ''.join(binlist)[::-1]
