5adf1f7583e79ca90a67154ac6d3377094871836
DrFrasierCrane <tylersartin@gmail.com>
2013-03-06 19:47:58 -0500

    Cleaned up documentation for both revbin and dec2bin

--- dec2bin.py
+++ dec2bin.py
@@ -1,18 +1,19 @@
-## Code written by Tyler Sartin on 18 February 2013
-def dec2bin(num):	
+def dec2bin(num):
+	''' This function takes in a decimal integer and then converts it
+		to binary
+		INPUT: Decimal integer
+		OUTPUT: Binary'''
+	num = int(num)	
 	if num == 0:
 		print 0
 	elif num < 0:
 		raise ValueError, "Please input a positive integer!"
 	else:
 		binlist = []
-	
+
 		while num > 0:
 			binlist.insert(0, num % 2)
 			num /= 2
-		
+
 		binlist = int(''.join(str(i) for i in binlist))
-		print binlist
-
-
-dec2bin(int(raw_input()))
+		return binlist
--- revbin.py
+++ revbin.py
@@ -1,28 +1,27 @@
-## Code written by Tyler Sartin on 18 February 2013.
-## Start the tool and check for inputs less than or equal to zero.
-def start(num):	
+def start(num):
+	''' Begins the process by checking input 'num' is a positive, non-zero
+		integer '''
 	if num == 0:
 		print 0
 	elif num < 0:
 		raise ValueError, "Please input a positive integer!"
 	else:
-		convert(num)
-		
+		revbin(num)
 
 
-def convert(input):
+def revbin(input):
+	''' Converts decimal to binary and reverses the value
+		INPUT: Decimal integer
+		OUTPUT: Reversed binary'''
 	binlist = []
-	
+
 	while input > 0:
 		binlist.insert(0, input % 2)
 		input /= 2
-	
+
 	new = []
-	
+
 	for i in range(len(binlist)):
 		new.append(binlist[i] * 2 ** i)
-	
-	print sum(new)
 
-
-start(int(raw_input()))
+	return sum(new)
